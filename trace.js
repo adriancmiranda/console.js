04259ff061d5e2f6491bfe7ed9af23749b44aef6
Adrian Miranda <adriancmiranda@gmail.com>
2014-07-25 22:53:40 -0300

    Create trace.js

---
+++ trace.js
@@ -0,0 +1,30 @@
+(function (window) {
+    'use strict';
+    
+    (function (console) {
+        var ctor = function () {};
+        for (var methods='assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,timeStamp,profile,profileEnd,time,timeEnd,trace,warn'.split(','), method; method = methods.pop();) {
+            console[method] = console[method] || ctor;
+        }
+    }(function () {
+        try {
+            console.log();
+            return window.console;
+        } catch(error) {
+            return window.console = {};
+        }
+    }()));
+
+    window.trace = function () {
+        trace.history = trace.history || [];
+        trace.history.push(arguments);
+        if (window.console) {
+            var params = [].slice.call(arguments);
+            if (typeof console.log === 'object') {
+                trace.apply.call(console.log, console, params);
+            } else {
+                console.log.apply(console, params);
+            }
+        }
+    };
+}(this));
