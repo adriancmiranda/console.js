6e2aa4814f211ef3ac5cee23549a45632c1018ab
Adrian Miranda <adriancmiranda@gmail.com>
2015-04-01 10:41:36 -0300

    Update trace.js

--- trace.js
+++ trace.js
@@ -15,7 +15,7 @@
             factory(global, true) :
             function(w) {
                 if (!w.document) {
-                    throw new Error("jQuery requires a window with a document");
+                    throw new Error("trace requires a window with a document");
                 }
                 return factory(w);
             };
