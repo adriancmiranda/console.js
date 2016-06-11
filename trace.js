b0fa57c61a1fd56ba7fe51eb46a7fda9aca73da8
Adrian C. Miranda <adriancmiranda@gmail.com>
2016-06-11 16:14:02 -0300

    Refactoring: easy peasy to read

--- trace.js
+++ trace.js
@@ -1,53 +1,87 @@
 /**
  * @author Adrian C. Miranda <adriancmiranda@gmail.com>
  * @see https://github.com/adriancmiranda/console.js
- * @version 1.0.4
+ * @version 1.1.0
  */
 (function(global, factory){
-	'use strict';
+  'use strict';
 
-	if(typeof module === 'object' && typeof module.exports === 'object'){
-		module.exports = factory(global, true);
-	}else{
-		global.trace = factory(global);
-	}
+  if(typeof module === 'object' && typeof module.exports === 'object'){
+    module.exports = factory(global, true);
+  }else{
+    global.trace = factory(global).log;
+  }
 
-}(typeof window !== 'undefined' ? window : this, function(window, nodeEnv){
-	'use strict';
+}(typeof window !== 'undefined' ? window : this, function(global, nodeEnv){
+  'use strict';
 
-	var debug = /\bdebug\b/.test(window.location.href);
+  var debug = !nodeEnv && /\bdebug\b/.test(global.location.href);
+  var slice = Array.prototype.slice;
 
-	(function(console){
-		console.enabled = debug || console.enabled;
-		var slice = Array.prototype.slice;
-		var ctor = function(){};
-		for(var methods = 'assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,timeStamp,profile,profileEnd,time,timeEnd,trace,warn,log'.split(','), method; method = methods.pop();){
-			(function(logger, method){
-				console[method] = function(){
-					if(console.enabled){
-						var params = slice.call(arguments);
-						if(console.history.length >= console.scrollback){
-							console.history.shift();
-						}
-						console.history.push({ type:method, message:params });
-						return logger.apply(console, params);
-					}
-				};
-			}).call(console, console[method] || ctor, method);
-		}
-	}(function(limit){
-		try{
-			console.log();
-			window.console.history = [];
-			window.console.scrollback = limit;
-			return window.console;
-		}catch(error){
-			window.console = { history:[], scrollback:limit };
-			return window.console;
-		}
-	}(window.console && typeof window.console.scrollback === 'number'?
-		window.console.scrollback : 10
-	)));
+  function isDefined(value){
+    return typeof value !== 'undefined';
+  }
 
-	return console.log;
+  function backtrace(opts){
+  }
+
+  function getNativeStackTrace(error){
+    return error.stack || error['opera#sourceloc'];
+  }
+
+  function parseStackTrace(opts, error){
+    if(isDefined(error.stacktrace) || isDefined(error['opera#sourceloc'])){
+    }else if(error.stack){
+    }
+  }
+
+  function getStackTrace(){
+    try{
+      throw new Error();
+    }catch(error){
+      if(getNativeStackTrace(error)){
+        return parseStackTrace(opts, error);
+      }
+      return backtrace(opts);
+    }
+  }
+
+  function getMethodNameByIndex(index){
+    return '';
+  }
+
+  function trace(console, outputFn, method){
+    console[method] = function(){
+      if(!console.enabled) return void(0);
+      var prefix = getMethodNameByIndex(-1);
+      var args = [prefix? prefix+':' : prefix].concat(slice.call(arguments));
+      if(console.history.length >= console.scrollback){
+        console.history.shift();
+      }
+      console.history.push({ type:method, message:args });
+      return outputFn.apply(console, args);
+    };
+  }
+
+  function Logger(console){
+    var ctor = function(){};
+    console.enabled = debug || console.enabled;
+    for(var methods = 'assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,timeStamp,profile,profileEnd,time,timeEnd,trace,warn,log'.split(','), method; method = methods.pop();){
+      trace.call(console, console, console[method] || ctor, method);
+    }
+  }
+
+  return Logger(function(limit){
+    try{
+      console.log();
+      global.console.history = [];
+      global.console.scrollback = limit;
+      return global.console;
+    }catch(error){
+      global.console = { history:[], scrollback:limit };
+      return global.console;
+    }
+  }(global.console && typeof global.console.scrollback === 'number'?
+    global.console.scrollback : 10
+  ));
 }));
